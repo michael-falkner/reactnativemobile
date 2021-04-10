3cabc3de3dc641773405d83cd4dcabced5ea4231
Michael Falkner <michael.allen.falkner@gmail.com>
2021-04-10 10:10:15 -0400

    Week 1 Work Shop Assignment---Added About Us and Contact Us components

---
+++ components/ContactComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import { Card, Text } from 'react-native-elements';
+
+class Contact extends Component {
+
+    
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+
+render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+        <ScrollView>
+            <Card 
+            title='Contact Us'
+            wrapperStyle={{margin: 20}}
+            >
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+
+            </Card>
+        </ScrollView>
+            );
+}
+}
+
+export default Contact;
