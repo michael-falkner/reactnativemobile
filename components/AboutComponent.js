358c766ad53b36d596f4873e9feec0a018f277fc
Michael Falkner <michael.allen.falkner@gmail.com>
2021-04-14 20:11:53 -0400

    Activyt Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,8 @@ import { ScrollView, FlatList, Text } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -32,16 +34,38 @@ class About extends Component {
     }
 
     render() {
-        const { navigate } = this.props.navigation;
         const renderPartner = ({item}) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView style={{backgroundColor:'#e9e9ef'}}>
                 <Mission />
