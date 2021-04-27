e2d5d6a22cdf6496edddc1b39dbc398b9e6fad4d
Michael Falkner <michael.allen.falkner@gmail.com>
2021-04-27 17:56:26 -0400

    Sending Email and Sharing functions

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
-import { Card, Text } from 'react-native-elements';
+import { Card, Button, Icon, Text } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
+
 
 
 class Contact extends Component {
@@ -12,7 +14,13 @@ class Contact extends Component {
         title: 'Contact Us'
     }
 
-
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 render() {
     const { navigate } = this.props.navigation;
 
@@ -28,6 +36,18 @@ render() {
                 <Text style={{marginBottom: 10}}>U.S.A.</Text>
                 <Text>Phone: 1-206-555-1234</Text>
                 <Text>Email: campsites@nucamp.co</Text>
+                <Button
+                    title="Send Email"
+                    buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                    icon={<Icon
+                            name='envelope-o'
+                            type='font-awesome'
+                            color='#fff'
+                            iconStyle={{marginRight: 10}}
+                          />}
+                          onPress={() => this.sendMail()}
+                    />
+               
 
             </Card>
             </Animatable.View>
